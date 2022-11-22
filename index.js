eabee85ce8fb47a9e874f15135cbe144a7446bd8
mrdivinemaniac <divinemaniac@gmail.com>
2022-11-22 14:02:25 +0545

    Add version to api response

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const HTTP_PORT = process.env.HTTP_PORT;
 if (!HTTP_PORT) throw new Error("HTTP_PORT is required");
 
 app.get("/", (req, res) => {
-  res.send(`This is running on port ${HTTP_PORT}`);
+  res.send(`This app is version 1.0.0 and listening on port ${HTTP_PORT}`);
 });
 
 const server = app.listen(HTTP_PORT, () => {
-  console.log(`App listening on port ${HTTP_PORT}`);
+  console.log(`App is version 1.0.0 and listening on port ${HTTP_PORT}`);
   process.send("ready");
 });
 
